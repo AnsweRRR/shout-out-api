e1b603f9201842bae0a25d5d0962567968c1a89e
Tamás Pogrányi <pogranyitamas99@gmail.com>
2023-12-12 14:59:03 +0100

    small edits

--- shout-out-api/Hubs/SignalRHub.cs
+++ shout-out-api/Hubs/SignalRHub.cs
@@ -14,11 +14,29 @@ namespace shout_out_api.Hubs
         public override async Task OnConnectedAsync()
         {
             var cancellationToken = Context.ConnectionAborted;
+            var connectionId = Context.ConnectionId;
 
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"CancellationToken: ${cancellationToken}");
+            Console.WriteLine($"ConnectionId: ${connectionId}");
             Console.WriteLine("Client connected to the hub. Welcome :)");
-            Console.WriteLine(cancellationToken);
+            Console.WriteLine("-------------------");
 
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var cancellationToken = Context.ConnectionAborted;
+            var connectionId = Context.ConnectionId;
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"CancellationToken: ${cancellationToken}");
+            Console.WriteLine($"ConnectionId: ${connectionId}");
+            Console.WriteLine("$Client disconnected from the hub. Bye :(");
+            Console.WriteLine("-------------------");
+
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
